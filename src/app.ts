8c799ebb1fd0872afd6594f556fe578ad041b015
Diego Pineda <zentoo31@gmail.com>
2025-07-16 09:28:39 -0500

    Add celebratory emoji to server startup log message

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,5 @@ app.use("/auth", authRouter);
 
 app.use(errorHandler);
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🎉 Server is running on http://localhost:${PORT}`);
 });
