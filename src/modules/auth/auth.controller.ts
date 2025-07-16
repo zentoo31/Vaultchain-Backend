707b91a3825558728aeaed00589aed5380442c4b
Diego Pineda <zentoo31@gmail.com>
2025-07-16 01:12:12 -0500

    Add AuthController for user registration functionality

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { AuthService } from "./auth.service";
+import { Request, Response } from "express";
+import { RegisterDTO } from "./dto/register.dto";
+
+export class AuthController {
+    private authService: AuthService;
+
+    constructor() {
+        this.authService = new AuthService();
+    }
+
+    register = async (req: Request, res: Response) => {
+        const registerDto = new RegisterDTO();
+        registerDto.email = req.body.email;
+        registerDto.password = req.body.password;
+
+        const user = await this.authService.registerUser(registerDto);
+        res.status(201).json(user);
+    }
+}
