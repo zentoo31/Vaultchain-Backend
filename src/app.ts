ade08fef1f969b0b9be424c7df8bf0391bef4063
Diego Pineda <zentoo31@gmail.com>
2025-07-20 13:27:27 -0500

    refactor: reorganize imports and add middleware for cookie parsing and CORS

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from "express";
-import { PORT } from "./config/constants";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./modules/auth/auth.route";
+import profileInfoRouter from "./modules/profile-info/profile-info.route";
 import { errorHandler } from "./middlewares/errorHandler";
 import { rateLimiter } from "./middlewares/rateLimiter";
+import { PORT } from "./config/constants";
+import { corsOptions } from "./config/corsOptions";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(rateLimiter);
 
 app.use("/auth", authRouter);
+app.use("/profile", profileInfoRouter);
 
 app.use(errorHandler);
 app.listen(PORT, () => {
