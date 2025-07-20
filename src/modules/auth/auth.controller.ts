f35877c61b7304f91c959acc886e245369eb5156
Diego Pineda <zentoo31@gmail.com>
2025-07-20 13:27:47 -0500

    feat: implement login and logout functionality with JWT token handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,10 @@
 import { AuthService } from "./auth.service";
 import { Request, Response } from "express";
 import { RegisterDTO } from "./dto/register.dto";
+import { LoginDTO } from "./dto/login.dto";
+import jwt, { Secret } from "jsonwebtoken";
+import { JWT_SECRET } from "../../config/constants";
+
 
 export class AuthController {
     private authService: AuthService;
@@ -16,5 +20,38 @@ export class AuthController {
 
         await this.authService.registerUser(registerDto);
         res.status(201).json({ message: "User registered successfully" });
+    }
+
+    login = async (req: Request, res: Response) => {
+        const loginDto = new LoginDTO();
+        loginDto.email = req.body.email;
+        loginDto.password = req.body.password;
+
+        const user = await this.authService.loginUser(loginDto);
+
+        const token = jwt.sign(
+            { id: user.id },
+            JWT_SECRET as Secret,
+            {expiresIn: '1h'}
+        );
+        
+        res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+                maxAge: 3600000
+            })
+            .status(200)
+            .json({
+                message: "Login successful",
+                token: token
+            });
+    }
+
+    logout = async (req: Request, res: Response) => {
+        res
+            .clearCookie('token')
+            .status(200)
+            .json({ message: "Logged out successfully" });
     }
 }
