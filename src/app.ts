133b2e6eecfc718dc8bc66e959d0c7b5e320caf0
Diego Pineda <zentoo31@gmail.com>
2025-07-21 14:20:54 -0500

    feat: add morgan middleware for logging HTTP requests

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,19 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./modules/auth/auth.route";
+import morgan from "morgan";
 import profileInfoRouter from "./modules/profile-info/profile-info.route";
 import { errorHandler } from "./middlewares/errorHandler";
 import { rateLimiter } from "./middlewares/rateLimiter";
 import { corsOptions } from "./config/corsOptions";
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(rateLimiter);
+app.use(morgan("dev"));
 
 app.use("/auth", authRouter);
 app.use("/profile", profileInfoRouter);
