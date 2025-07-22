d00ed62de430a63c336f4de8dfd4bbd1117fdc64
Diego Pineda <zentoo31@gmail.com>
2025-07-21 23:43:15 -0500

    refactor: restructure server setup to use HTTP server with Socket.IO

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import app from "./app";
+import { setSocketServer } from "./utils/socket";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { PORT } from "./config/constants";
+import { corsOptions } from "./config/corsOptions";
+import { setUpChatSocket } from "./sockets/chat.socket";
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: corsOptions
+});
 
-app.listen(PORT, () => {
-    console.log(`🎉 Server is running on http://localhost:${PORT}`);
+setSocketServer(io);
+setUpChatSocket(io);
+
+httpServer.listen(PORT, () => {
+    console.log(`🎉 Server is running on port ${PORT}`);
 });
