1d071a87679c145e46b933382ff28bbc487a3632
Diego Pineda <zentoo31@gmail.com>
2025-08-16 20:50:41 -0500

    feat: add authentication middleware to verify-token route for enhanced security

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { catchAsync } from "../../middlewares/catchAsync";
+import { authenticateToken } from "../../middlewares/auth";
 
 const authRouter = Router();
 const authController = new AuthController();
 
 authRouter.post("/register", catchAsync(authController.register));
 authRouter.post("/login", catchAsync(authController.login));
-authRouter.get("/verify-token", catchAsync(authController.verifyToken))
+authRouter.get("/verify-token", authenticateToken , catchAsync(authController.verifyToken))
 authRouter.post("/logout", catchAsync(authController.logout));
 
 export default authRouter;
