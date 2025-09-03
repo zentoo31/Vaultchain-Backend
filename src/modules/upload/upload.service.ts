a6ba6c07bd752de96b01e8ae88b24d9382a84997
zentoo31 <zentoo31@gmail.com>
2025-09-03 13:13:46 -0500

    feat: initialize PrismaClient in UploadService constructor

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,7 +1,12 @@
 import multer from "multer";
 import crypto from "crypto";
 import path from "path";
+import { PrismaClient } from "@prisma/client/extension";
 
 export class UploadService{
+    private prisma: PrismaClient;
     
+    constructor(){
+        this.prisma = new PrismaClient();
+    }
 }
